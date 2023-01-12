4572cc5ba2eb62fcb8aea3bc0414180d31084f61
KotaroYuzawa <ktr.yzw@gmail.com>
2023-01-12 19:11:06 +0100

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,13 @@
+<?php
+echo '
+<nav id="nav" class="nav">
+      <div class="nav-container">
+        <ul>
+          <li><a href="/kotaroyuzawa/" style="opacity: 1;">About</a></li>
+          <li><a href="/kotaroyuzawa/quereinstieg">Quereinstieg</a></li>
+          <li><a href="/kotaroyuzawa/FAQ">FAQ</a></li>
+          <li><a href="/kotaroyuzawa/contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    ';
